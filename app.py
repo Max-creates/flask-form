3bcb7b6cb262d932a41dc8d886843f3f98084dbc
Max <maksym.solomyanov@gmail.com>
2024-05-07 11:08:14 +0200

    Store the user data in the database, add submission notification, send confirmation email.

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+from datetime import datetime
+import os
+password = os.getenv("PASSWORD")
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "myapplication"
+app.config["SECRET_KEY"] = "myapplication"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "max.solo213@gmail.com"
+app.config["MAIL_PASSWORD"] = password
 
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,7 +34,26 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
-        last_name = request.form["occupation"]
+        date_obj = datetime.strptime(date, '%Y-%m-%d')
+        occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
+        flash(f"{first_name}, your form was submitted successfully!", "success")
+
+        message_body = (f"Thank you for your submission, {first_name}. \n"
+                        f"Here are your data:\n{first_name}\n{last_name}\n{date}\n"
+                        f"Thank you!")
+
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+
+
     return render_template('index.html')
 
 
